f12950f4b3d4ec308845afc603ea7a4fb61926e2
DesnLee <jiakun.ui@gmail.com>
2021-03-20 12:43:14 +0800

    格式化代码

--- server.js
+++ server.js
@@ -3,33 +3,47 @@ var fs = require('fs')
 var url = require('url')
 var port = process.argv[2]
 
-if(!port){
+if (!port) {
     console.log('请指定端口号! 像这样 node server.js 8888')
     process.exit(1)
 }
 
-var server = http.createServer(function(request, response){
+var server = http.createServer(function (request, response) {
     var parsedUrl = url.parse(request.url, true)
     var pathWithQuery = request.url
     var queryString = ''
-    if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+    if (pathWithQuery.indexOf('?') >= 0) {
+        queryString = pathWithQuery.substring(pathWithQuery.indexOf('?'))
+    }
     var path = parsedUrl.pathname
     var query = parsedUrl.query
     var method = request.method
 
     /******** 从这里开始看，上面不要看 ************/
 
-    console.log('有个人摸了一下 ' + pathWithQuery +' 这个地方')
+    console.log('有个人摸了一下 ' + pathWithQuery + ' 这个地方')
 
-    if(path === '/'){
+    if (path === '/') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/html;charset=utf-8')
         response.write(`
-        <link rel="stylesheet" href="./style.css">
-        <h1>你好，这是我的Node.js</h1>
+        <!doctype html>
+        <html lang="zh-CN">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport"
+                  content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <link rel="stylesheet" href="./style.css">
+            <title>我的Node.js页面</title>
+        </head>
+        <body>
+            <h1>你好，这是我的Node.js</h1>
+        </body>
+        </html>
         `)
         response.end()
-    } else if(path === '/style.css'){
+    } else if (path === '/style.css') {
         response.statusCode = 200
         response.setHeader('Content-Type', 'text/css;charset=utf-8')
         response.write(`
